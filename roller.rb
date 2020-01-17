0fdf77f51fa555592c3c6150c069fed310d38af7
hiswhiteraven <hiswhiteraven@protonmail.com>
2020-01-17 11:00:51 -0500

    Change range in method from exclusive to inclusive

--- roller.rb
+++ roller.rb
@@ -13,14 +13,25 @@ EOC
 
 def roll_die(how_many_dice)
   # Rolling just one die
-  random_sided_die = rand(5...6)
+  random_sided_die = rand(5..6)
   puts "Rolling a #{random_sided_die} sided die!"
   rolling_die = rand(random_sided_die) + 1
+  puts rolling_die
+
+  puts "-----"
 
   # Rolling numerous dice
-  number_of_dies = how_many_dice
-  puts "Rolling #{how_many_dice} #{random_sided_die} sided dice!"
+  number_of_dies = { "1" => "one", "2" => "two", "3" => "three", "4" => "four", "5" => "five", "6" => "six" }
+  random_sided_die_second_roll = rand(5..6)
+  dice_selection = number_of_dies[how_many_dice]
+  puts "Rolling #{dice_selection} #{random_sided_die_second_roll} sided dice!"
+  dice_roll_possibility = how_many_dice * random_sided_die_second_roll
+  puts dice_roll_possibility
 
 end
 
 roll_die(5)
+roll_die(4)
+roll_die(3)
+roll_die(2)
+roll_die(1)
