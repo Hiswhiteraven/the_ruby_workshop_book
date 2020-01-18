bd4ec18e6e416029a99202de4570f1fcba4529e7
hiswhiteraven <hiswhiteraven@protonmail.com>
2020-01-18 18:28:57 -0500

    If conditional modified to case statement

--- speed_determiner.rb
+++ speed_determiner.rb
@@ -1,5 +1,7 @@
+<<EOC
 def drive_decision(traffic_signal, weather, distance_to_signal, speed_limit)
 
+  
   if traffic_signal == :green
 
     if weather == :sunny
@@ -9,6 +11,7 @@ def drive_decision(traffic_signal, weather, distance_to_signal, speed_limit)
     else
       return speed_limit * 0.90
     end
+ 
 
   elsif traffic_signal == :yellow
 
@@ -37,6 +40,77 @@ def drive_decision(traffic_signal, weather, distance_to_signal, speed_limit)
   end
 
 end
+EOC
 
-p drive_decision(:green, :sunny, 100, 50)
-p drive_decision(:yellow, :rainy, 50, 25)
+def drive_decision_when_green(weather, distance_to_signal, speed_limit)
+
+  case weather
+  when :sunny
+    return speed_limit
+  when :rainy
+    if distance_to_signal >= 50
+      return speed_limit
+    else
+      return speed_limit * 0.90
+    end
+  else
+    raise "Not handled"
+  end
+
+end
+
+def drive_decision_when_yellow(weather, distance_to_signal, speed_limit)
+
+  case weather
+  when :sunny
+    if distance_to_signal >= 50
+      return speed_limit * 0.80
+    else
+      return speed_limit * 0.50
+    end
+  when :rainy
+    if distance_to_signal >= 50
+      return speed_limit
+    else
+      return speed_limit * 0.90
+    end
+  else
+    raise "Not handled"
+  end
+
+end
+
+def drive_decision_when_red(weather, distance_to_signal, speed_limit)
+
+  if distance_to_signal >= 50
+    case weather
+    when :sunny
+      speed_limit * 0.50
+    when :rainy
+      speed_limit * 0.25
+    else
+      raise "Not handled"
+    end
+  else
+    return 0
+  end
+
+end
+
+def drive_decision(traffic_signal, weather, distance_to_signal, speed_limit)
+
+  case traffic_signal
+  when :green
+    drive_decision_when_green(weather, distance_to_signal, speed_limit)
+  when :yellow
+    drive_decision_when_yellow(weather, distance_to_signal, speed_limit)
+  else
+    drive_decision_when_red(weather, distance_to_signal, speed_limit)
+  end
+
+end
+
+p drive_decision(:yellow, :sunny, 25, 35)
+p drive_decision(:red, :rainy, 25, 35)
+p drive_decision(:green, :rainy, 25, 35)
+p drive_decision(:green, :sunny, 25, 35)
