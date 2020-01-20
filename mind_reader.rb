38ee5874692b58414682dcc1b787889b31d7f0ff
hiswhiteraven <hiswhiteraven@protonmail.com>
2020-01-20 17:20:12 -0500

    Define mind_reader method

--- mind_reader.rb
+++ mind_reader.rb
@@ -1,3 +1,15 @@
-def mind_reader
+def mind_reader(range)
+
+  magic_number = rand(range)
+  puts "What's your guess?"
+  guess = gets.chomp.to_i
+
+  if guess == magic_number
+    puts "That's right!"
+  else
+    puts "Sorry, that's not correct. The correct number is: #{magic_number}"
+  end
 
 end
+
+mind_reader(1..3)
