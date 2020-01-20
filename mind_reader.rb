9dcd4f70171ef8c6db035bea3d13accfef6e5618
hiswhiteraven <hiswhiteraven@protonmail.com>
2020-01-20 17:25:49 -0500

    Add unless conditional statement and modify method

--- mind_reader.rb
+++ mind_reader.rb
@@ -1,3 +1,4 @@
+<<-EOC
 def mind_reader(range)
 
   magic_number = rand(range)
@@ -7,9 +8,27 @@ def mind_reader(range)
   if guess == magic_number
     puts "That's right!"
   else
-    puts "Sorry, that's not correct. The correct number is: #{magic_number}"
+    puts "Sorry, that's not correct. The correct number is: # {magic_number}"
   end
 
 end
 
 mind_reader(1..3)
+EOC
+
+def mind_reader
+
+  magic_number = 7
+  guess = nil
+
+  while guess != magic_number do
+    print "Nope! Try Again! " unless guess.nil?
+    puts "What's your guess?"
+    guess = gets.chomp.to_i
+  end
+
+  puts "That's right!"
+
+end
+
+mind_reader
