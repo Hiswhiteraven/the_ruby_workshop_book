a7e9e3019a20eac8c7fc75a297316e4294cc4c20
hiswhiteraven <hiswhiteraven@protonmail.com>
2020-01-20 18:12:56 -0500

    Add logic for three guesses

--- mind_reader.rb
+++ mind_reader.rb
@@ -19,15 +19,29 @@ EOC
 def mind_reader
 
   magic_number = 7
+  max_guesses = 3
+  attempts = 0
   guess = nil
 
   while guess != magic_number do
     print "Nope! Try Again! " unless guess.nil?
     puts "What's your guess?"
     guess = gets.chomp.to_i
+    break if attempts >= max_guesses
+    attempts = attempts + 1
   end
 
-  puts "That's right!"
+  winner = "You've guessed it!"
+  loser = "You ran out of guesses, try again later!"
+  puts guess == magic_number ? winner : loser
+
+    # puts guess == magic_number ? "That's right!" : "You ran out of guesses, try again later!"
+
+    # if guess == magic_number
+    # puts "That's right!"
+    # else
+    # puts "You ran out of guesses, try again later!"
+    # end
 
 end
 
