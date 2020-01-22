bab2876c9773e5f3ef9c19800fe690910423658d
hiswhiteraven <hiswhiteraven@protonmail.com>
2020-01-22 16:48:28 -0500

    Refactor mind_reader method to utilize loop

--- mind_reader.rb
+++ mind_reader.rb
@@ -16,24 +16,33 @@ end
 mind_reader(1..3)
 EOC
 
-def mind_reader
+def mind_reader magic_number
 
-  magic_number = 7
-  max_guesses = 3
-  attempts = 0
+  max_attempts = 7
+  attempt = 0
   guess = nil
 
-  while guess != magic_number do
-    print "Nope! Try Again! " unless guess.nil?
-    puts "What's your guess?"
+  loop do
+
+    print "What's your guess? "
     guess = gets.chomp.to_i
-    break if attempts >= max_guesses
-    attempts = attempts + 1
+    break if attempt >= max_attempts
+    break if guess == magic_number
+    attempt = attempt + 1
+    puts "Nope! Try again"
+
   end
 
-  winner = "You've guessed it!"
-  loser = "You ran out of guesses, try again later!"
-  puts guess == magic_number ? winner : loser
+  if guess == magic_number
+    puts "That's right!"
+  else
+    puts "You ran out of guesses, try again later!"
+  end
+
+
+  # winner = "You've guessed it!"
+  # loser = "You ran out of guesses, try again later!"
+  # puts guess == magic_number ? winner : loser
 
     # puts guess == magic_number ? "That's right!" : "You ran out of guesses, try again later!"
 
@@ -45,4 +54,4 @@ def mind_reader
 
 end
 
-mind_reader
+mind_reader(7)
