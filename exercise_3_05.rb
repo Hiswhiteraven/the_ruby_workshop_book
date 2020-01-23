01d8a5c412a1310ead9e5a8f3200e6bc5dc457c6
hiswhiteraven <hiswhiteraven@protonmail.com>
2020-01-22 22:49:18 -0500

    Modify exercise_3_05.rb - process three variables

--- exercise_3_05.rb
+++ exercise_3_05.rb
@@ -1,10 +1,11 @@
 def math(array)
 
   first_item = array.shift
-  while(array.length > 0) do
+  while(array.length > 1) do
 
-    second_item = array.shift
-    first_item = yield first_item, second_item
+    second_item = array.shift || 0
+    third_item = array.shift || 0
+    first_item = yield first_item, second_item, third_item
 
   end
 
@@ -12,4 +13,4 @@ def math(array)
 
 end
 
-p math([4,5,6]) { |a,b| a + b}
+p math([3,4,5]) { |a,b,c| a + b + c}
