e1e7dbab9c77e1d94fced132482ddd06d30c5363
hiswhiteraven <hiswhiteraven@protonmail.com>
2020-01-22 22:46:51 -0500

    Add exercise_03_05.rb file to git

---
+++ exercise_3_05.rb
@@ -0,0 +1,15 @@
+def math(array)
+
+  first_item = array.shift
+  while(array.length > 0) do
+
+    second_item = array.shift
+    first_item = yield first_item, second_item
+
+  end
+
+  return first_item
+
+end
+
+p math([4,5,6]) { |a,b| a + b}
